e7082949d295459c10b915e648bc173bfcdac10e
Cainã <cainamwc@gmail.com>
2025-09-15 23:49:07 -0300

    arquivos ainda incompletos

---
+++ JavaBootcamp/src/bootcamp/exercicios4/questao1/Cinema.java
@@ -0,0 +1,20 @@
+package bootcamp.exercicios4.questao1;
+
+public class Cinema {
+	public static void main(String[] args) {
+		Bilheteria bilheteria = new Bilheteria();
+		
+		bilheteria.addFilme(new Filme("1917", 40));
+		bilheteria.addFilme(new Filme("Alita - Anjo de Combate", 35));
+		bilheteria.addFilme(new Filme("Coraline", 40));
+		bilheteria.addFilme(new Filme("Vingadores - Guerra Infinita", 50));
+		bilheteria.addFilme(new Filme("Atividade Paranormal", 30));
+		bilheteria.addFilme(new Filme("Guerra Mundial Z", 30));
+		bilheteria.addFilme(new Filme("Rec", 20));
+		
+		while (true) {
+			
+		}
+	}
+
+}
