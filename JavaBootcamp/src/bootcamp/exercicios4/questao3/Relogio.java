c8ba3f7198e0be98c7d9cc8494c89a38377bb20c
Cainã <cainamwc@gmail.com>
2025-09-24 02:28:29 -0300

    metodos criados

--- JavaBootcamp/src/bootcamp/exercicios4/questao3/Relogio.java
+++ JavaBootcamp/src/bootcamp/exercicios4/questao3/Relogio.java
@@ -1,5 +1,30 @@
 package bootcamp.exercicios4.questao3;
 
-public sealed class Relogio permits RelogioBrasileiro, RelogioAmericano {
-
+public sealed abstract class Relogio permits RelogioBrasileiro, RelogioAmericano {
+	private int hora;
+	private int minuto;
+	private int segundo;
+	
+	public abstract String getHorario();
+	
+	public int getHora() {
+		return hora;
+	}
+	public void setHora(int hora) {
+		this.hora = hora;
+	}
+	public int getMinuto() {
+		return minuto;
+	}
+	public void setMinuto(int minuto) {
+		this.minuto = minuto;
+	}
+	public int getSegundo() {
+		return segundo;
+	}
+	public void setSegundo(int segundo) {
+		this.segundo = segundo;
+	}
+	
+	
 }
